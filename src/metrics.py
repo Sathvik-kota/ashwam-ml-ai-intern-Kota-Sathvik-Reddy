eb827854297be86a72c55ba5ecf280ce70fc890f
Sathvik <kotasathvik25@gmail.com>
2026-01-02 08:13:53 +0530

    Agreement rate metric function

--- src/metrics.py
+++ src/metrics.py
@@ -1,1 +1,6 @@
-s
+def agreement_rate(groups, total_runs=3):
+    stable = 0
+    for g in groups:
+        if len(g["objects"]) == total_runs:
+            stable += 1
+    return stable / len(groups) if groups else 0.0
